fb3ee22be11be08076447c8914d9656d6b09bfb6
Arun Kumar <arunkumar7617@gmail.com>
2019-09-04 20:19:46 +0400

    Updates

---
+++ precache-manifest.8d7f02af5ccd672f6e43517799c488b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c13adccfdb8dda5e49a44c65100fd40",
+    "url": "/chatbot/index.html"
+  },
+  {
+    "revision": "be219c5dc0b0ffe8b2b7",
+    "url": "/chatbot/static/css/main.cb79800b.chunk.css"
+  },
+  {
+    "revision": "c67810554023e7938ce4",
+    "url": "/chatbot/static/js/2.7105bc32.chunk.js"
+  },
+  {
+    "revision": "be219c5dc0b0ffe8b2b7",
+    "url": "/chatbot/static/js/main.cdab9e28.chunk.js"
+  },
+  {
+    "revision": "a81bbe6677bee1f58ff6",
+    "url": "/chatbot/static/js/runtime~main.6cefce26.js"
+  }
+]);
